af6b3a93e622a2f7ab5932b6d880127ac9b37b58
RyanLin5967 <idident5967@gmail.com>
2026-05-02 14:20:19 -0400

    added a leaf icon

---
+++ frontend/src/components/LeafIcon.jsx
@@ -0,0 +1,8 @@
+export default function LeafIcon({ size = 28 }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 21c1-4 3-7 7-9" stroke="#7fba6a" strokeWidth="1.5" strokeLinecap="round"/>
+      <path d="M11 5c5-.5 9 1 11 4-2 5-6 8-11 9C7 17 4 14 3 9c3-3 6-4 8-4z" stroke="#7fba6a" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill="#7fba6a" fillOpacity="0.1"/>
+    </svg>
+  )
+}
